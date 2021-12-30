f73e00eae59a00062c0ab5187d2049e3a8f71837
mustafamertkisa <m.mertkisa@gmail.com>
2021-12-30 21:25:44 +0300

    Düzenlemeler yapıldı

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,31 +1,46 @@
 import React, { useState } from "react";
+import "./styles.css";
 
 const INITIAL_STATE = [
   { id: 1, title: "Kitap oku", status: false },
-  { id: 2, title: "Temizlik yap", status: true },
+  { id: 2, title: "Temizlik yap", status: false },
 ];
 
 export default function App() {
   const [list, setList] = useState(INITIAL_STATE);
+  const [newTask, setNewTask] = useState("");
 
   return (
     <div className="App">
       <h1>React To-Do List App</h1>
 
       <div className="addForm">
-        <input placeholder="Add list" />
-        <button>Add</button>
+        <input
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add list"
+        />
+        <button
+          onClick={() => {
+            setList([
+              ...list,
+              { id: Date.now(), title: newTask, status: false },
+            ]);
+            setNewTask("");
+          }}
+        >
+          Add
+        </button>
       </div>
 
       <div className="list">
-        {list.map((item) => (
+        {list.map((item, index) => (
           <div
+            key={index}
             onClick={() => {
               setList(
-                list.map((element) =>
-                  element.id === item.id
-                    ? { ...element, status: !element.status }
-                    : element.status
+                list.map((el) =>
+                  el.id === item.id ? { ...el, status: !el.status } : el.status
                 )
               );
             }}
@@ -35,6 +50,13 @@ export default function App() {
           </div>
         ))}
       </div>
+
+      <button
+        onClick={() => setList(list.filter((item) => !item.status))}
+        className="clear"
+      >
+        Clear completed
+      </button>
     </div>
   );
 }
